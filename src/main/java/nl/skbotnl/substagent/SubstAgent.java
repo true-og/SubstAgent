48c6c251d978e45774989dbf6445cddbca3df5fb
NotAlexNoyle <alexnoyle@icloud.com>
2025-11-12 13:31:11 -0500

    Fix conflicts between SKBotNL branch and true-og branch

--- src/main/java/nl/skbotnl/substagent/SubstAgent.java
+++ src/main/java/nl/skbotnl/substagent/SubstAgent.java
@@ -7,8 +7,8 @@ public class SubstAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
 
         String version = SubstAgent.class.getPackage().getImplementationVersion();
-        System.out.println("[SubstAgent] Loaded version " + version);
         inst.addTransformer(new BufferedReaderTransformer());
+        System.out.println("[SubstAgent] Loaded version " + version);
 
     }
 
